22092a3d5fe801201b210539e14a7ca192f10c43
Neha Bisht <neha.bisht356@gmail.com>
2025-05-15 23:42:10 -0700

    Make some more changes

--- TabDrawer/Home/HomeViewController.swift
+++ TabDrawer/Home/HomeViewController.swift
@@ -9,8 +9,43 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let menuViewController = MenuViewController()
+    var isSideViewVisible = false
+    
+    var leadingConstraint: NSLayoutConstraint?
+    var menuWidth: CGFloat = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .purple
+        menuWidth = view.frame.width * 0.8
+        addChild(menuViewController)
+        menuViewController.view.frame = self.view.frame
+        view.addSubview(menuViewController.view)
+        menuViewController.didMove(toParent: self)
+        if let sideView = menuViewController.view {
+            sideView.translatesAutoresizingMaskIntoConstraints = false
+            let leadingCo = sideView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -menuWidth)
+            leadingConstraint = leadingCo
+            NSLayoutConstraint.activate([
+                leadingCo,
+                sideView.widthAnchor.constraint(equalToConstant: menuWidth),
+                sideView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                sideView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            ])
+        }
+    }
+    
+    @IBAction func showSideView(_ id: UIBarButtonItem) {
+        UIView.animate(withDuration: 0.3) {
+            if self.isSideViewVisible {
+                self.leadingConstraint?.constant = -self.menuWidth
+            } else {
+                self.leadingConstraint?.constant = 0
+            }
+            self.view.layoutIfNeeded()
+        } completion: { success in
+            if success {
+                self.isSideViewVisible.toggle()
+            }
+        }
     }
 }
